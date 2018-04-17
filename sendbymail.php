c6069b415cda6607e1233a231ef88771cdd829c7
Steevin <danielsteevin@gmail.com>
2018-04-16 22:37:03 -0500

    diseño full

---
+++ sendbymail.php
@@ -0,0 +1,33 @@
+<?php
+if(isset($_POST['email'])) {
+
+// Debes editar las próximas dos líneas de código de acuerdo con tus preferencias
+$email_to = "danielsteevin@icloud.com";
+$email_subject = "Contacto desde el sitio web";
+
+// Aquí se deberían validar los datos ingresados por el usuario
+if(!isset($_POST['nombre']) ||
+!isset($_POST['correo']) ||
+!isset($_POST['descripcion']) ||
+!isset($_POST['telephone'])) {
+
+echo "<b>Ocurrió un error y el formulario no ha sido enviado. </b><br />";
+echo "Por favor, vuelva atrás y verifique la información ingresada<br />";
+die();
+}
+
+$email_message = "Detalles del formulario de contacto:\n\n";
+$email_message .= "Nombre: " . $_POST['nombre'] . "\n";
+$email_message .= "E-mail: " . $_POST['correo'] . "\n";
+$email_message .= "Comentarios: " . $_POST['descripcion'] . "\n\n";
+
+
+// Ahora se envía el e-mail usando la función mail() de PHP
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);
+
+echo "¡El formulario se ha enviado con éxito!";
+}
+?>
